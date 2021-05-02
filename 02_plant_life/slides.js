f937a515ab96f59f236b3f3c5c63e94131c883b2
laurajunco <lc.junco10@uniandes.edu.co>
2021-05-02 18:09:06 -0500

    Add slides.js

---
+++ 02_plant_life/slides.js
@@ -0,0 +1,33 @@
+const slideArea = document.querySelector("div.slides")
+const images = slideArea.querySelectorAll("img")
+let currentSlide = 0
+let z = 1
+
+slideArea.addEventListener("click", function() {
+  currentSlide++
+  z++
+
+  if(currentSlide > images.length - 1) {
+      images.forEach(image => {
+      image.style.animation = "";
+    })
+    currentSlide = 0
+  }
+
+  images[currentSlide].style.zIndex = z;
+  images[currentSlide].style.animation = "fade 0.5s";
+})
+
+slideArea.addEventListener("mouseover", function() {
+  images.forEach(image => {
+    const x = 25 * Math.floor(Math.random() * 5) - 50;
+    const y = 25 * Math.floor(Math.random() * 5) - 50;
+    image.style.transform = `translate(${x}px, ${y}px)`  ;
+  })
+})
+
+slideArea.addEventListener("mouseout", function() {
+  images.forEach(image => {
+    image.style.transform = ""  ;
+  })
+})
