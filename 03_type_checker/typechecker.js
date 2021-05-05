758ecd83697199455402047213cf476383d7685c
laurajunco <lc.junco10@uniandes.edu.co>
2021-05-04 20:34:20 -0500

    Continuar proyecto 3

--- 03_type_checker/typechecker.js
+++ 03_type_checker/typechecker.js
@@ -1,11 +1,44 @@
 const sentenceTag = document.querySelector(`input[type="text"`)
-const outputTag = document.querySelector(".output")
-const originalText = outputTag.innerHTML
+const typesizeTag = document.querySelector(`input[name="typesize"`)
+const leadingTag = document.querySelector(`input[name="leading"`)
+const italicTag = document.querySelector(`input[type="checkbox"`)
+
+const typesizeOutput = document.querySelector("span.typesize-output")
+const leadingOutput = document.querySelector("span.leading-output")
+
+const outputTag = document.querySelector("textarea.output")
+const originalText = outputTag.value
 
 sentenceTag.addEventListener("keyup", function() {
   if(this.value) {
-    outputTag.innerHTML = this.value
+    outputTag.value = this.value
   } else {
-    outputTag.innerHTML= originalText
+    outputTag.value= originalText
+  }
+})
+
+outputTag.addEventListener("keyup", function() {
+  if(this.value) {
+    sentenceTag.value = this.value
+  } else {
+    sentenceTag.value= originalText
+  }
+})
+
+typesizeTag.addEventListener("input", function() {
+  outputTag.style.fontSize = `${this.value}px`
+  typesizeOutput.innerHTML = `${this.value}px`
+})
+
+leadingTag.addEventListener("input", function() {
+  outputTag.style.lineHeight = this.value
+  leadingOutput.innerHTML = this.value
+})
+
+italicTag.addEventListener("change", function() {
+  if(this.checked) {
+    outputTag.style.fontStyle = "italic"
+  } else {
+    outputTag.style.fontStyle = "normal"
   }
 })
