76bafac4ac1506b7434c38f638fd7e402e277618
laurajunco <lc.junco10@uniandes.edu.co>
2021-05-05 17:30:25 -0500

    Proyecto 3 =^)

--- 03_type_checker/typechecker.js
+++ 03_type_checker/typechecker.js
@@ -1,11 +1,15 @@
-const sentenceTag = document.querySelector(`input[type="text"`)
 const typesizeTag = document.querySelector(`input[name="typesize"`)
 const leadingTag = document.querySelector(`input[name="leading"`)
 const italicTag = document.querySelector(`input[type="checkbox"`)
+const fontweightTag = document.querySelector(`input[name="font-weight"`)
+const typefaceTag = document.querySelector(`select[name="typeface"`)
 
 const typesizeOutput = document.querySelector("span.typesize-output")
 const leadingOutput = document.querySelector("span.leading-output")
+const fontweightOutput = document.querySelector("span.font-weight-output")
 
+const colorTags = document.querySelectorAll("div.colors div")
+const sentenceTag = document.querySelector(`input[type="text"`)
 const outputTag = document.querySelector("textarea.output")
 const originalText = outputTag.value
 
@@ -35,10 +39,31 @@ leadingTag.addEventListener("input", function() {
   leadingOutput.innerHTML = this.value
 })
 
+fontweightTag.addEventListener("input", function() {
+  outputTag.style.fontWeight = this.value
+  fontweightOutput.innerHTML = this.value
+})
+
 italicTag.addEventListener("change", function() {
   if(this.checked) {
     outputTag.style.fontStyle = "italic"
   } else {
     outputTag.style.fontStyle = "normal"
   }
+})
+
+typefaceTag.addEventListener("input", function() {
+  outputTag.style.fontFamily = this.value
+})
+
+colorTags.forEach(tag => {
+  tag.addEventListener("click", function() {
+    outputTag.style.color = this.style.color
+    outputTag.style.backgroundColor = this.style.backgroundColor
+
+    colorTags.forEach(tag => {
+      tag.classList.remove("selected")
+    })
+    this.classList.add("selected")
+  })
 })
