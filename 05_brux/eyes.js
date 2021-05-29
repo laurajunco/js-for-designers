ae40344053cf0ca15e8ebb242bfc965c11c4374a
Laura Junco <lc.junco10@uniandes.edu.co>
2021-05-29 16:43:35 -0500

    Start to setup animation

---
+++ 05_brux/eyes.js
@@ -0,0 +1,10 @@
+const irisLeft = document.querySelector("div.iris-left")
+const irisRight = document.querySelector("div.iris-right")
+
+const moveEye = (tag, mouseX, mouseY) => {
+
+}
+
+document.addEventListener('mousemove', (event) => {
+  
+})
